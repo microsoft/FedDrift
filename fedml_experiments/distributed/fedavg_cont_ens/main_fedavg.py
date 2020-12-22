93e44a8eb51f58d71e0b00ba8bda331d0143fd5c
Kevin Hsieh <kevin.hsieh@microsoft.com>
2020-12-21 16:24:13 -0800

    add FedAvg with ensemble model

---
+++ fedml_experiments/distributed/fedavg_cont_ens/main_fedavg.py
@@ -0,0 +1,237 @@
+import argparse
+import logging
+import os
+import socket
+import sys
+
+import numpy as np
+import psutil
+import setproctitle
+import torch
+import wandb
+
+# add the FedML root directory to the python path
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../../")))
+
+from fedml_api.data_preprocessing.sea.data_loader import load_partition_data_sea
+
+from fedml_api.data_preprocessing.sine.data_loader import load_partition_data_sine
+
+from fedml_api.data_preprocessing.circle.data_loader import load_partition_data_circle
+
+from fedml_api.model.linear.lr import LogisticRegression
+
+from fedml_api.model.fnn.fnn import FeedForwardNN
+
+from fedml_api.distributed.fedavg_ens.FedAvgEnsAPI import FedML_init, FedML_FedAvgEns_distributed
+
+
+def add_args(parser):
+    """
+    parser : argparse.ArgumentParser
+    return a parser added with args required by fit
+    """
+    # Training settings
+    parser.add_argument('--model', type=str, default='mobilenet', metavar='N',
+                        help='neural network used in training')
+
+    parser.add_argument('--dataset', type=str, default='cifar10', metavar='N',
+                        help='dataset used for training')
+
+    parser.add_argument('--data_dir', type=str, default='./../../../data/cifar10',
+                        help='data directory')
+
+    parser.add_argument('--client_num_in_total', type=int, default=1000, metavar='NN',
+                        help='number of workers in a distributed cluster')
+
+    parser.add_argument('--client_num_per_round', type=int, default=4, metavar='NN',
+                        help='number of workers')
+
+    parser.add_argument('--batch_size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+
+    parser.add_argument('--client_optimizer', type=str, default='adam',
+                        help='SGD with momentum; adam')
+
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='learning rate (default: 0.001)')
+
+    parser.add_argument('--wd', help='weight decay parameter;', type=float, default=0.001)
+
+    parser.add_argument('--epochs', type=int, default=5, metavar='EP',
+                        help='how many epochs will be trained locally')
+
+    parser.add_argument('--comm_round', type=int, default=10,
+                        help='how many round of communications we shoud use')
+
+    parser.add_argument('--is_mobile', type=int, default=0,
+                        help='whether the program is running on the FedML-Mobile server side')
+
+    parser.add_argument('--frequency_of_the_test', type=int, default=1,
+                        help='the frequency of the algorithms')
+
+    parser.add_argument('--gpu_server_num', type=int, default=1,
+                        help='gpu_server_num')
+
+    parser.add_argument('--gpu_num_per_server', type=int, default=4,
+                        help='gpu_num_per_server')
+
+    parser.add_argument('--ci', type=int, default=0,
+                        help='CI')
+
+    parser.add_argument('--total_train_iteration', type=int, default=3,
+                        help='The number of FedML iterations (over time)')
+
+    parser.add_argument('--curr_train_iteration', type=int, default=0,
+                        help='The current Fededrated Learning iterations (over time)')
+
+    parser.add_argument('--drift_together', type=int, default=0,
+                        help='If the concept drift happens at the same time across all clients')
+
+    parser.add_argument('--report_client', type=int, default=0,
+                        help='Whether reporting the accuracy of each client')
+
+    parser.add_argument('--retrain_data', type=str, default='all',
+                        help='which data to be included for retraining')
+
+    parser.add_argument('--concept_drift_algo', type=str, default='aue',
+                        help='The algorithm to handle concept drift')
+
+    args = parser.parse_args()
+    return args
+
+
+def load_data(args, dataset_name):
+    logging.info("load_data. dataset_name = %s" % dataset_name)
+    
+    if dataset_name == "sea":        
+        client_num, train_data_num, test_data_num, train_data_global, test_data_global, \
+        train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
+        class_num = load_partition_data_sea(args.batch_size, args.curr_train_iteration,
+                                            args.client_num_in_total, args.retrain_data)
+        feature_num = 3
+
+    elif dataset_name == "sine":
+        client_num, train_data_num, test_data_num, train_data_global, test_data_global, \
+        train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
+        class_num = load_partition_data_sine(args.batch_size, args.curr_train_iteration,
+                                             args.client_num_in_total, args.retrain_data)
+        feature_num = 2
+
+    elif dataset_name == "circle":
+        client_num, train_data_num, test_data_num, train_data_global, test_data_global, \
+        train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
+        class_num = load_partition_data_circle(args.batch_size, args.curr_train_iteration,
+                                               args.client_num_in_total, args.retrain_data)
+        feature_num = 2
+
+    dataset = [train_data_num, test_data_num, train_data_global, test_data_global,
+               train_data_local_num_dict, train_data_local_dict, test_data_local_dict,
+               class_num, feature_num]
+    return dataset
+
+
+def create_model(args, model_name, output_dim, feature_dim):
+    logging.info("create_model. model_name = %s, output_dim = %s" % (model_name, output_dim))
+    model = None
+    if model_name == "lr":
+        logging.info("LogisticRegression, feature_dim = %s" % feature_dim)
+        model = LogisticRegression(feature_dim, output_dim)
+    if model_name == "fnn":
+        logging.info("FeedForwardNN, feature_dim = %s" % feature_dim)
+        model = FeedForwardNN(feature_dim, output_dim, feature_dim * 2)
+    return model
+
+
+def init_training_device(process_ID, fl_worker_num, gpu_num_per_machine):
+    # initialize the mapping from process ID to GPU ID: <process ID, GPU ID>
+    if process_ID == 0:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        return device
+    process_gpu_dict = dict()
+    for client_index in range(fl_worker_num):
+        gpu_index = client_index % gpu_num_per_machine
+        process_gpu_dict[client_index] = gpu_index
+
+    logging.info(process_gpu_dict)
+    device = torch.device("cuda:" + str(process_gpu_dict[process_ID - 1]) if torch.cuda.is_available() else "cpu")
+    logging.info(device)
+    return device
+
+
+if __name__ == "__main__":
+    # initialize distributed computing (MPI)
+    comm, process_id, worker_number = FedML_init()
+
+    logging.basicConfig(filename='output.log', level=logging.INFO)
+
+    # parse python script input parameters
+    parser = argparse.ArgumentParser()
+    args = add_args(parser)
+    logging.info(args)
+#    args.curr_train_iteration -= 1
+
+    # customize the process name
+    str_process_name = "FedAvg (distributed):" + str(process_id)
+    setproctitle.setproctitle(str_process_name)
+
+    # customize the log format
+    logging.basicConfig(level=logging.INFO,
+                        format=str(
+                            process_id) + ' - %(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                        datefmt='%a, %d %b %Y %H:%M:%S')
+    hostname = socket.gethostname()
+    logging.info("#############process ID = " + str(process_id) +
+                 ", host name = " + hostname + "########" +
+                 ", process ID = " + str(os.getpid()) +
+                 ", process Name = " + str(psutil.Process(os.getpid())))
+
+    # initialize the wandb machine learning experimental tracking platform (https://www.wandb.com/).
+    if process_id == 0:
+        wandb.init(
+            project="fedml",
+            name="FedAvgCont(d)-" + args.dataset +
+                "-r" + str(args.comm_round) + "-e" +
+                str(args.epochs) + "-lr" + str(args.lr) +
+                "-iter" + str(args.curr_train_iteration) +
+                "-dt" + str(args.drift_together) +
+                "-" + args.retrain_data,
+            config=args
+        )
+
+    # Set the random seed. The np.random seed determines the dataset partition.
+    # The torch_manual_seed determines the initial weight.
+    # We fix these two, so that we can reproduce the result.
+    np.random.seed(0)
+    torch.manual_seed(10)
+
+    # GPU arrangement: Please customize this function according your own topology.
+    # The GPU server list is configured at "mpi_host_file".
+    # If we have 4 machines and each has two GPUs, and your FL network has 8 workers and a central worker.
+    # The 4 machines will be assigned as follows:
+    # machine 1: worker0, worker4, worker8;
+    # machine 2: worker1, worker5;
+    # machine 3: worker2, worker6;
+    # machine 4: worker3, worker7;
+    # Therefore, we can see that workers are assigned according to the order of machine list.
+    logging.info("process_id = %d, size = %d" % (process_id, worker_number))
+    device = init_training_device(process_id, worker_number - 1, args.gpu_num_per_server)
+
+    # load data
+    dataset = load_data(args, args.dataset)
+    [train_data_num, test_data_num, train_data_global, test_data_global,
+     train_data_local_num_dict, train_data_local_dict, test_data_local_dict,
+     class_num, feature_num] = dataset
+
+    # create model.
+    # Note if the model is DNN (e.g., ResNet), the training will be very slow.
+    model = create_model(args, model_name=args.model, output_dim=class_num,
+                         feature_dim = feature_num)
+
+    # start "federated averaging (FedAvg) with ensembled" for this round
+    FedML_FedAvgEns_distributed(process_id, worker_number, device, comm,
+                                model, train_data_num, train_data_global,
+                                test_data_global, train_data_local_num_dict,
+                                train_data_local_dict, test_data_local_dict,
+                                args)
